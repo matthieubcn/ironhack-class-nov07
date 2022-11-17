c17f61e78512771fc025cf5281e697e4f36648f5
Matthieu Thuillier <thuillier.matthieu@hotmail.fr>
2022-11-17 07:21:04 +0100

    done

--- labs/js/lab-dom-ironhack-cart-master/js/index.js
+++ labs/js/lab-dom-ironhack-cart-master/js/index.js
@@ -1,30 +1,47 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const priceElement = product.querySelector('.price span');
+  const quantityElement = product.querySelector('.quantity input');
 
-  //... your code goes here
-}
+  const price = priceElement.innerText;
+  const quantity = quantityElement.value;
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  const subtotalElement = product.querySelector('.subtotal span');
+  const subtotal = price * quantity;
+  subtotal= subtotalElement.innerText;
+
+  return subtotal;
+
+console.log (updateSubtotal())
+
+  function calculateAll() {
+    // code in the following two lines is added just for testing purposes.
+    // it runs when only iteration 1 is completed. at later point, it can be removed.
+  
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const multipleProducts = document.querySelectorAll('.product');
+  let total = 0;
+  for (let product of multipleProducts) {
+    total += updateSubtotal(product);
+  }
+}
 
   // ITERATION 3
   //... your code goes here
-}
+}  const totalPriceElement = document.querySelector('#total-value span');
+totalPriceElement.innerText = total;
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  const productElement = target.parentElement.parentElement;
+  const tableBodyElement = productElement.parentElement;
+  tableBodyElement.removeChild(productElement);
+
   //... your code goes here
 }
 
